ffc04349933e22d33730cab47789d876689bf329
Osekobo <collinsboseko2005@gmail.com>
2026-01-31 23:22:44 +0300

    myapi.py added

--- SortNames.java
+++ SortNames.java
@@ -1,25 +1,22 @@
 import java.util.ArrayList;
 import java.util.Collections;
-// import java.util.List;
-
 public class SortNames {
-
     public static void main(String[] args) {
-
         // Create an ArrayList of names
         ArrayList<String> names = new ArrayList<>();
-        names.add("Collins");
-        names.add("Cyprian");
-        names.add("David");
-        names.add("Peter");
-        names.add("Joseph");
-        names.add("Kemboi");
-        names.add("Joel");
-        names.add("Kelvin");
-        names.add("Philip");
-        names.add("Rono");
-        names.add("Masea");
-        names.add("Leparan");
+        names.add("Collins Oseko EEEI/02388/2024");
+        names.add("Cyprian Kirui EEI/02336/2024");
+        names.add("David Omega EEEI/02357/2024");
+        names.add("Peter Otieno EEEI/01375/2022");
+        names.add("Joseph Mwaniki EEEI/02329/2024");
+        names.add("Gidion Kemboi EEEI/02335/2024");
+        names.add("Joel Mutuku EEEI/02310/2024");
+        names.add("Kelvin Kiprono EEEI/02362/2024");
+        names.add("Philip Mwanzia EEEI/01518/2024");
+        names.add("Rono Collins EEEI/02334/2024");
+        names.add("Masea Bellamy EEEI/02367/2024");
+        names.add("Leparan Duncan EEEI/02338/2024");
+        names.add("Dennis Mwangi EEEI/02967/2024");
 
         // Display original list
         System.out.println("Original List:");
@@ -30,10 +27,6 @@ public class SortNames {
 
         System.out.println("\nThe names in ascending order are:");
         System.out.println(names);
-
-        // Add more names to demonstrate List.sort()
-        // names.add("Eve");
-        // names.add("Frank");
 
         // -------- Method 2: Using List.sort() --------
         names.sort(null); // null means natural ordering (ascending)
